e774a8703e74b0892a2062c1057ab0d1b7cfb222
Daniel Eshel <esheld23@gmail.com>
2025-04-18 16:47:15 -0400

    continued working on peer.py testing

--- peer.py
+++ peer.py
@@ -12,10 +12,14 @@ async def get_available_peers(comms: Comms):
                     available_peers.append(peer)
 
 
-def get_input(msg, comms):
+async def get_input(msg, comms):
     user_in = input(msg)
     print(user_in)
-    comms.start_connection_info(user_in)
+    await comms.start_connection_info(user_in)
+    for i in range(20):
+        await asyncio.sleep(1)
+
+
 
 
 async def main():
@@ -31,8 +35,7 @@ async def main():
 
     comms = Comms("Daniel", interface_name)
     await comms.start()
-    await asyncio.to_thread(get_input, "choose peer to connect to: ", comms)  # Keep running for demo
-
+    await get_input("", comms=comms)
 
 if __name__ == "__main__":
     asyncio.run(main())
